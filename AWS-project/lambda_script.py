579eef0125b308bbefa45dae6e154fcc32ddc111
xmass6 <144550199+xmass6@users.noreply.github.com>
2024-02-02 17:03:49 -0500

    First comit

---
+++ AWS-project/lambda_script.py
@@ -0,0 +1,11 @@
+import json
+import boto3
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('count')
+def lambda_handler(event, context):
+     response = table.get_item(Key={'VisitorCount':'1'})
+     views = response['Item']['views']
+     views = views +1 
+     print(views)
+     response = table.put_item(Item={'VisitorCount':'1', 'views':views})
+     return views
---
+++ AWS-project/viewcount.js
@@ -0,0 +1,8 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+	let response = await fetch("https://5o2yfwbmy9.execute-api.ca-central-1.amazonaws.com/dev/cloud-resume-api")
+	let data = await response.json();
+	counter.innerHTML = `Visitor: ${data}`;
+}
+
+updateCounter();
