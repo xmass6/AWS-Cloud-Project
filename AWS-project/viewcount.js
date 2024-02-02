a85073ef457066768b65c2f763f55de9d0a36ab4
xmass6 <144550199+xmass6@users.noreply.github.com>
2024-02-02 18:13:11 -0500

    Update viewcount.js

--- AWS-project/viewcount.js
+++ AWS-project/viewcount.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
 	let response = await fetch("https://5o2yfwbmy9.execute-api.ca-central-1.amazonaws.com/dev/cloud-resume-api")
 	let data = await response.json();
-	counter.innerHTML = `Visitor count: ${data}`;
+	counter.innerHTML = `Visitors: ${data}`;
 }
 
 updateCounter();
